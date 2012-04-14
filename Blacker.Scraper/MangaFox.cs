2554477d00b904a171ade9cbd403d0eb4956d6f7
Lukáš Černý <cerny.l@gmail.com>
2012-04-14 20:22:38 +0200

    MangaFox scraper implementation

---
+++ Blacker.Scraper/MangaFox.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using Blacker.Scraper.Cache;
+using Blacker.Scraper.Models;
+using Blacker.Scraper.Helpers;
+using Blacker.Scraper.Exceptions;
+
+namespace Blacker.Scraper
+{
+    public sealed class MangaFox : BaseScraper, IScraper, IImmediateSearchProvider, IPreload
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(MangaFox));
+
+        private const string DictionaryUrl = "http://mangafox.me/manga";
+        private const string MangaFoxUrl = "http://mangafox.me";
+
+        private Cache<string, object> _cache;
+
+        private const string MangasCacheKey = "Mangas";
+        private const string ChapterCacheKey = "Chapters";
+
+        public MangaFox()
+        {
+            _cache = new Cache<string, object>();
+            HtmlAgilityPack.HtmlNode.ElementsFlags.Remove("option");
+        }
+
+        protected override string BaseUrl
+        {
+            get { return MangaFoxUrl; }
+        }
+
+        private Guid Scraper
+        {
+            get { return Guid.Parse("bbc1ed62-3535-4131-9c00-5148dd2f2035"); }
+        }
+
+        #region IScraper implementation
+
+        public string Name { get { return "MangaFox.me"; } }
+
+        public IEnumerable<ChapterRecord> GetAvailableChapters(MangaRecord manga)
+        {
+            if (manga == null)
+                throw new ArgumentNullException("manga");
+            if (manga.Scraper != Scraper)
+                throw new ArgumentException("Manga record is not for " + Name, "manga");
+
+            var cacheKey = ChapterCacheKey + manga.MangaName + manga.Url;
+
+            var cached = _cache[cacheKey] as IEnumerable<ChapterRecord>;
+            if (cached != null) // if chapters are already in cache return them
+                return cached;
+
+            var records = new List<ChapterRecord>();
+            var document = WebHelper.GetHtmlDocument(manga.Url);
+
+            var chapters = document.SelectNodes(@"//ul[@class=""chlist""]/li//h3|//ul[@class=""chlist""]/li//h4");
+            if (chapters == null)
+            {
+                throw new ParserException("Could not find expected elements on website.", document.InnerHtml);
+            }
+
+            foreach (var chapter in chapters)
+            {
+                records.Add(new ChapterRecord(Scraper)
+                {
+                    MangaName = manga.MangaName,
+                    ChapterName = CleanupText(chapter.InnerText),
+                    Url = GetFullUrl(chapter.ChildNodes.FirstOrDefault(n => n.Name == "a").Attributes["href"].Value),
+                    MangaRecord = manga
+                });
+            }
+
+            // save to cache
+            _cache[cacheKey] = records;
+
+            return records;
+        }
+
+        public IEnumerable<MangaRecord> GetAvailableMangas(string filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException("filter");
+
+            return Mangas.Where(mr => mr.MangaName.IndexOf(filter, StringComparison.InvariantCultureIgnoreCase) != -1);
+        }
+
+        public IDownloader GetDownloader()
+        {
+            return new Downloader(GetPages, @"//img[@id=""image""]");
+        }
+
+        #endregion // IScraper implementation
+
+        #region IImmediateSearchProvider implementation
+
+        public IEnumerable<MangaRecord> GetAvailableMangasImmediate(string filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException("filter");
+
+            return Mangas.Where(mr => mr.MangaName.IndexOf(filter, StringComparison.InvariantCultureIgnoreCase) != -1);
+        }
+
+        #endregion // IImediateSearchProvider implementation
+
+        #region IPreload implementation
+
+        public void PreloadDirectory()
+        {
+            Mangas.FirstOrDefault();    // this will load mangas to cache if they are not already loaded
+        }
+
+        #endregion // IPreload implementation
+
+        #region Private properties
+
+        private IEnumerable<MangaRecord> Mangas
+        {
+            get
+            {
+                var mangas = _cache[MangasCacheKey] as IEnumerable<MangaRecord>;
+                if (mangas == null)
+                    _cache[MangasCacheKey] = LoadAllMangas();
+
+                return _cache[MangasCacheKey] as IEnumerable<MangaRecord>;
+            }
+        }
+
+        #endregion // Private properties
+
+        #region Private methods
+
+        private IEnumerable<MangaRecord> LoadAllMangas()
+        {
+            var records = new List<MangaRecord>();
+            var document = WebHelper.GetHtmlDocument(DictionaryUrl);
+
+            var mangas = document.SelectNodes(@"//div[@class=""manga_list""]//ul/li/a");
+            if (mangas == null)
+            {
+                throw new ParserException("Could not find expected elements on website.", document.InnerHtml);
+            }
+
+            foreach (var manga in mangas)
+            {
+                if (string.IsNullOrEmpty(manga.InnerText))
+                    continue;
+
+                records.Add(new MangaRecord(Scraper)
+                {
+                    MangaName = CleanupText(manga.InnerText),
+                    Url = GetFullUrl(manga.Attributes["href"].Value)
+                });
+            }
+
+            return records;
+        }
+
+        private IDictionary<int, string> GetPages(ChapterRecord chapter)
+        {
+            IDictionary<int, string> pages = new Dictionary<int, string>();
+
+            var document = WebHelper.GetHtmlDocument(chapter.Url);
+
+            var commentsLink = document.SelectSingleNode(@"//a[@id=""comments""]");
+            if (commentsLink == null)
+            {
+                throw new ParserException("Could not find expected elements on website.", document.InnerHtml);
+            }
+
+            var link = commentsLink.Attributes["href"].Value;
+
+            var chapterPages = document.SelectNodes(@"//select[@class=""m""][1]/option");
+            if (chapterPages == null)
+            {
+                throw new ParserException("Could not find expected elements on website.", document.InnerHtml);
+            }
+
+            foreach (var pageLink in chapterPages)
+            {
+                int pageNumber = 0;
+
+                if (!Int32.TryParse(pageLink.InnerText, out pageNumber))
+                    continue;
+
+                if (pages.ContainsKey(pageNumber)) // if page is already in dictionary then skip adding it
+                    continue;
+
+                pages.Add(pageNumber, link + pageNumber + ".html");
+            }
+
+            return pages;
+        }
+
+        #endregion // Private methods
+    }
+}
