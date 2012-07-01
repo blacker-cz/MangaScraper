98c6ef808db97efc01080a3d8459d9f0e5f20ff5
Lukáš Černý <cerny.l@gmail.com>
2012-07-01 18:51:49 +0200

    Capture download time in DEBUG diagnostics

--- Blacker.Scraper/Helpers/WebHelper.cs
+++ Blacker.Scraper/Helpers/WebHelper.cs
@@ -4,11 +4,15 @@ using System.Net;
 using System.IO;
 using Blacker.Scraper.Exceptions;
 using System.Drawing;
+using log4net;
+using System.Diagnostics;
 
 namespace Blacker.Scraper.Helpers
 {
     internal static class WebHelper
     {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(WebHelper));
+
         /// <summary>
         /// Get HTML node of the remote website
         /// </summary>
@@ -24,12 +28,19 @@ namespace Blacker.Scraper.Helpers
             HtmlDocument doc = new HtmlDocument();
             try
             {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
                 var request = HttpWebRequest.Create(url);
                 using (var response = request.GetResponse())
                 using (var stream = response.GetResponseStream())
                 {
                     doc.Load(stream, true);
                 }
+
+                stopWatch.Stop();
+
+                _log.DebugFormat("Download of URL '{0}' took '{1}'", url, stopWatch.Elapsed);
 
                 return doc.DocumentNode;
             }
@@ -54,12 +65,19 @@ namespace Blacker.Scraper.Helpers
 
             try
             {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
                 var request = HttpWebRequest.Create(imageUrl);
                 using (var response = request.GetResponse())
                 using (var stream = response.GetResponseStream())
                 {
                     MemoryStream memoryStream = new MemoryStream();
                     stream.CopyTo(memoryStream);
+
+                    stopWatch.Stop();
+                    _log.DebugFormat("Download of image from URL '{0}' took '{1}'", imageUrl, stopWatch.Elapsed);
+                    
                     return new Bitmap(memoryStream);
                 }
             }
