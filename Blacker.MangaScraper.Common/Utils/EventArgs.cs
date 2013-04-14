0719f185424433097d7b8006beb599943b843228
Lukáš Černý <cerny.l@gmail.com>
2013-04-14 18:56:49 +0200

    Add generic EventArgs class

---
+++ Blacker.MangaScraper.Common/Utils/EventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Blacker.MangaScraper.Common.Utils
+{
+    public class EventArgs<T> : EventArgs
+    {
+        public EventArgs(T value)
+        {
+            _value = value;
+        }
+
+        private readonly T _value;
+
+        public T Value
+        {
+            get { return _value; }
+        }
+    }
+}
