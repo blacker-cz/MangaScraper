11a839b0f9a275ab607b6b93d31d3fe0c6ae2f39
Lukáš Černý <cerny.l@gmail.com>
2013-10-31 22:09:06 +0100

    Show date/time when the chapter was downloaded

---
+++ MangaScraper/Converters/DateTimeFormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Blacker.MangaScraper.Converters
+{
+    public class DateTimeFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((DateTime) value == DateTime.MinValue)
+                return string.Empty;
+            
+            return ((DateTime) value).ToString((string) parameter);
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
